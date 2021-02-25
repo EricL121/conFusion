1f0b62e498dce307e83179ac348ee91c8af3817e
EricL121 <el.mimic@hotmail.com>
2021-02-25 15:56:36 +0800

    Single Page Application Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,14 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { AppComponent } from './app.component';
 
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -44,7 +46,7 @@ import { ContactComponent } from './contact/contact.component';
     MatButtonModule,
     AppRoutingModule,
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
